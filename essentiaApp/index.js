fc1e1c0fbd2ae5a60503bcfb5561ce5b4d7bcab4
Damla Önder <79882950+damlaonder@users.noreply.github.com>
2023-12-02 22:35:00 -0500

    Add files via upload

---
+++ essentiaApp/index.js
@@ -0,0 +1,6 @@
+import "bootswatch/dist/[theme]/bootstrap.min.css";
+
+import React from 'react';
+import logo from './logo.svg';
+import 'bootswatch/dist/slate/bootstrap.min.css'; // Added this :boom:
+import './App.css';
