65eee60ae187899b3376f88fa668cf553d1b4df5
Damla Önder <79882950+damlaonder@users.noreply.github.com>
2023-12-05 01:19:43 -0500

    Delete essentiaApp directory

--- essentiaApp/index.js
+++
@@ -1,6 +0,0 @@
-import "bootswatch/dist/[theme]/bootstrap.min.css";
-
-import React from 'react';
-import logo from './logo.svg';
-import 'bootswatch/dist/slate/bootstrap.min.css'; // Added this :boom:
-import './App.css';
